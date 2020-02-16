e2b6d657e9d7435b6d82d5fc2e3d4b2f29f3b091
Lorenzo <lorenzorepetto28@gmail.com>
2020-02-16 19:46:10 -0300

    Organizacion en modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule } from "@angular/forms";
+// MODULES
+import { SharedModule } from './shared/shared.module';
+import { ChatModule } from './chat/chat.module';
+import { AppRoutingModule } from './app-routing.module';
 
+
+// SOCKETS
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-import { HttpClientModule } from "@angular/common/http";
-
 const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
 
-
-import { AppRoutingModule } from './app-routing.module';
+// COMPONENTS
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { LeftPanelComponent } from './chat/left-panel/left-panel.component';
-import { RightPanelComponent } from './chat/right-panel/right-panel.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { LoginComponent } from './auth/login/login.component';
-import { ChatComponent } from './chat/chat.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    LeftPanelComponent,
-    RightPanelComponent,
-    FooterComponent,
-    LoginComponent,
-    ChatComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
+    SharedModule,
+    ChatModule,
     SocketIoModule.forRoot(config)
   ],
   providers: [],
---
+++ src/app/chat/chat.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { SharedModule } from '../shared/shared.module';
+
+
+
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { RightPanelComponent } from './right-panel/right-panel.component';
+import { ChatComponent } from './chat.component';
+
+
+
+@NgModule({
+  declarations: [
+    ChatComponent,
+    LeftPanelComponent,
+    RightPanelComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    FormsModule,
+    HttpClientModule
+  ]
+})
+export class ChatModule { }
